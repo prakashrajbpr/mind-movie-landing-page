e8254fb35e7e05f42b8b06f4d995b9431ddb7fe3
prakashrajbpr <prakash1832001@gmail.com>
2025-06-17 23:02:38 -0500

    Add remix.config.js for proper Vercel deployment

---
+++ app/routes/remix.config.js
@@ -0,0 +1,17 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  ignoredRouteFiles: ["**/.*"],
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildPath: "build/server/index.js",
+  serverModuleFormat: "esm",
+};
