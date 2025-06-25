aff043579a29eb15cc2f0b8f63bfb285508f429f
prakashrajbpr <prakash1832001@gmail.com>
2025-06-25 12:58:02 -0500

    Simple UI with questions in the template

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import autoprefixer from "autoprefixer";
+import { vitePlugin as remix } from "@remix-run/dev";
 import tailwindcss from "tailwindcss";
-import path from "path";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "app"),
-    },
-  },
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
